1ce87a64510f717d3a521632f0fd911ef99b007a
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2025-04-07 16:57:28 +0000

    entrega ex1_recursividade

---
+++ Recursividade x Iteratividade/ex1_recursividade.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int contar_digitos(int n);
+
+int main(){
+    int n, qtd_digitos;
+
+    printf("Informe o número que deseja contar os Dígitos: ");
+    scanf("%d", &n);
+    
+    qtd_digitos = contar_digitos(n);
+
+    printf("O número %d possui %d dígitos.", n, qtd_digitos);
+
+
+    return 0;
+}
+
+int contar_digitos(int n){
+    n = abs(n);
+
+    if(n < 10){
+        return 1;
+    } else{
+        return 1 + contar_digitos(n/10); 
+    }
+
+}
