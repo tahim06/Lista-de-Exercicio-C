fdd17f6140270def4b1a12813315bcd106a5da65
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2025-04-10 23:06:54 +0000

    ex4_struct concluido

---
+++ Ex_Struct/ex4_struct.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Carro{
+    char modelo[31];
+    int ano;
+    float preco;
+}Carro;
+
+int main(){
+    Carro *carro = (Carro*)malloc(sizeof(Carro));
+
+    printf("Informe o Modelo do Carro: \n"); 
+    scanf("%30[^\n]", carro->modelo);
+    printf("Informe o Ano do Carro:\n");
+    scanf("%d", &carro->ano);
+    printf("Informe o Preço deste Modelo:\n");
+    scanf("%f", &carro->preco);
+
+    printf("\n----------Info Carro----------\n");
+    printf("Modelo do Carro: %s\n", carro->modelo);
+    printf("Ano do Carro: %d\n", carro->ano);
+    printf("Preço do Carro (ex: 20.000 - com o Ponto!!): %.3f\n", carro->preco);
+
+    free(carro);
+
+    return 0;
+}
