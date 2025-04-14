7fdecb980f045374169f44efda384678c248dfc2
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2025-04-14 15:24:00 +0000

    ex5_pilhas concluido

--- Listas, Pilhas e Filas/ex5_pilhas.c
+++ Listas, Pilhas e Filas/ex5_pilhas.c
@@ -14,21 +14,26 @@ void inicializarPilha(Pilha *pilha);
 No* criarNo(int valor);
 void push(Pilha *pilha, int valor);
 void pop(Pilha *pilha);
-void exibirTop(Pilha pilha);
+void exibirTop(Pilha *pilha);
+int isEmpty(Pilha *pilha);
+void limparPilha(Pilha *pilha);
 
 int main(){
     Pilha pilha;
     inicializarPilha(&pilha);
 
     push(&pilha, 10);
-    exibirTop(pilha);
+    exibirTop(&pilha);
     push(&pilha, 20);
-    exibirTop(pilha);
+    exibirTop(&pilha);
     push(&pilha, 30);
-    exibirTop(pilha);
+    exibirTop(&pilha);
 
     pop(&pilha);
-    exibirTop(pilha);
+    exibirTop(&pilha);
+
+    limparPilha(&pilha);
+    exibirTop(&pilha);
 }
 
 void inicializarPilha(Pilha *pilha){
@@ -50,7 +55,7 @@ No* criarNo(int valor){
 void push(Pilha *pilha, int valor){
     No* novoNo = criarNo(valor);
 
-    if(pilha->top == NULL){
+    if(isEmpty(pilha)){
         pilha->top = novoNo;
     }else{
         novoNo->prox = pilha->top;
@@ -60,17 +65,36 @@ void push(Pilha *pilha, int valor){
 }
 
 void pop(Pilha *pilha){
-    if(pilha->top == NULL){
+    if(isEmpty(pilha)){
         printf("Pilha está Vazia!\n\n");
         return;
     }else{
         No *temp = pilha->top;
         pilha->top = pilha->top->prox;
-        printf("Valor %d Removido do Topo da Lista!\n\n",temp->valor);
+        printf("Valor %d Removido do Topo da Pilha!\n\n",temp->valor);
         free(temp);
     }
 }
 
-void exibirTop(Pilha pilha){
-    printf("Topo da Lista: %d\n\n ", pilha.top->valor);
+void exibirTop(Pilha *pilha){
+    if(isEmpty(pilha)){
+        printf("Pilha Vazia!\n");
+        return;
+    }else{
+        printf("Topo da Pilha: %d\n\n ", pilha->top->valor);
+    }    
+}
+
+int isEmpty(Pilha *pilha){
+    if(pilha->top == NULL){
+        return 1;        
+    }else{
+        return 0;
+    }
+}
+
+void limparPilha(Pilha *pilha){
+    while (!isEmpty(pilha)) {
+        pop(pilha);
+    }
 }
